d67f9877e34f217ae2b42724891a30e9cdc3fc61
Kevin <468561207@qq.com>
2024-04-30 22:00:12 +0800

    release: 上传新版本

---
+++ dist/src/editor/dataset/enum/List.d.ts
@@ -0,0 +1,22 @@
+export declare enum ListType {
+    UL = "ul",
+    OL = "ol"
+}
+export declare enum UlStyle {
+    DISC = "disc",
+    CIRCLE = "circle",
+    SQUARE = "square",
+    CHECKBOX = "checkbox",
+    DRAWDISC = "drawdisc"
+}
+export declare enum OlStyle {
+    DECIMAL = "decimal"
+}
+export declare enum ListStyle {
+    DISC = "disc",
+    CIRCLE = "circle",
+    SQUARE = "square",
+    DECIMAL = "decimal",
+    CHECKBOX = "checkbox",
+    DRAWDISC = "drawdisc"
+}
